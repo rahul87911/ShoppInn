8694e3c3b2d021ad953692a67861daef64bed281
Rahul Mishra <afcatrahul8709@gmail.com>
2024-07-29 19:17:47 +0530

    implemented till home carousel

---
+++ frontend/src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,25 @@
+
+
+export const MainCarouselData = [
+    {
+        image: 'https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_5.jpg',
+        path: '/product1',
+    },
+    {
+        image: 'https://d6kigqwjl9u8w.cloudfront.net/mageplaza/bannerslider/banner/image/2/_/2_18.jpg',
+        path: '/product2',
+    },
+    {
+        image: 'https://d6kigqwjl9u8w.cloudfront.net/mageplaza/bannerslider/banner/image/4/_/4_19.jpg',
+        path: '/product3',
+    },
+    {
+        image: 'https://d6kigqwjl9u8w.cloudfront.net/mageplaza/bannerslider/banner/image/1/_/1_13.jpg',
+        path: '/product4',
+    },
+    {
+        image: 'https://d6kigqwjl9u8w.cloudfront.net/mageplaza/bannerslider/banner/image/3/_/3_20.jpg',
+        path: '/product5',
+    }
+];
+
---
+++ frontend/src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainCarousel from './../../components/HomeCarousel/MainCarousel';
+
+export default function HomePage() {
+  return (
+    <div>
+      <MainCarousel/>
+      <div>
+        others
+      </div>
+    </div>
+  )
+}
