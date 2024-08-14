71744c215a64cea411d54267d70cdc1f145cdd3d
Sagar <sagro93@gmail.com>
2024-08-14 20:54:54 +0530

    created method for create product

--- backend/src/main/java/com/shopInn/app/service/serviceImpl/ProductServiceImplementation.java
+++ backend/src/main/java/com/shopInn/app/service/serviceImpl/ProductServiceImplementation.java
@@ -1,11 +1,13 @@
 package com.shopInn.app.service.serviceImpl;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import com.shopInn.app.exception.ProductException;
+import com.shopInn.app.model.Category;
 import com.shopInn.app.model.Product;
 import com.shopInn.app.repo.CategoryRepository;
 import com.shopInn.app.repo.ProductRepository;
@@ -37,8 +39,60 @@ public class ProductServiceImplementation implements ProductService {
 	
 	@Override
 	public Product createProduct(CreateProductRequest req) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Category topLevel=categoryRepository.findByName(req.getTopLevelCategory()); 
+		
+		if(topLevel==null) {
+			Category topLevelCategory=new Category();
+			topLevelCategory.setName(req.getTopLevelCategory());
+			topLevelCategory.setLevel(1);
+			
+			topLevel=categoryRepository.save(topLevelCategory);
+		}
+		
+		
+		Category secondLevel=categoryRepository.findByNameAndParent(req.getSecondLevelCategory(),topLevel.getName());
+		
+		if(secondLevel==null) {
+			Category secondLevelCategory=new Category();
+			secondLevelCategory.setName(req.getSecondLevelCategory());
+			secondLevelCategory.setParentCategory(topLevel);
+			secondLevelCategory.setLevel(2);
+			
+			secondLevel=categoryRepository.save(secondLevelCategory);
+		}
+		
+		Category thirdLevel=categoryRepository.findByNameAndParent(req.getThirdLevelCategory(), secondLevel.getName());
+		if(thirdLevel==null)
+		{
+			Category thirdLevelCategory=new Category();
+			thirdLevelCategory.setName(req.getThirdLevelCategory());
+			thirdLevelCategory.setParentCategory(secondLevel);
+			thirdLevelCategory.setLevel(3);
+			
+			thirdLevel=categoryRepository.save(thirdLevelCategory);
+		}
+		
+		
+		
+		Product product=new Product();
+		product.setTitle(req.getTitle());
+		product.setColor(req.getColor());
+		product.setDescription(req.getDescription());
+		product.setDiscountedPrice(req.getDiscountedPrice());
+		product.setDiscountedPrice(req.getDiscountPercent());
+		product.setImageUrl(req.getImageUrl());
+		product.setBrand(req.getBrand());
+		product.setPrice(req.getPrice());
+		product.setSizes(req.getSize());
+		product.setQuantity(req.getQuantity());
+		product.setCategory(thirdLevel);
+		product.setCreatedAt(LocalDateTime.now());
+		
+		Product savedProduct=productRepository.save(product);
+		
+		
+		return savedProduct;
 	}
 
 	@Override
