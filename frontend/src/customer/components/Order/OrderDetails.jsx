5051b39fcf5d95fd6e094026263bb6e6fa309279
varunkhadse <varunkhadse7@gmail.com>
2024-08-14 16:53:34 +0530

    OrderDetails alignment bug fixed

--- frontend/src/customer/components/Order/OrderDetails.jsx
+++ frontend/src/customer/components/Order/OrderDetails.jsx
@@ -1,36 +1,75 @@
 import React from "react";
 import AddressCard from "../AddressCard/AddressCard";
-import { Add } from "@mui/icons-material";
-import OrderTracker from "./OrderTracker"
-import { Grid } from "@mui/material";
+import OrderTracker from "./OrderTracker";
+import { Box, Grid } from "@mui/material";
+import { deepPurple } from "@mui/material/colors";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+
 
 const OrderDetails = () => {
-    return(
-        <div className="px-5 lg:px-20">
-            <div>
-                <h1 className='font-semibold text-xl py-7'>Delivery address</h1>
-                <AddressCard/>
-            </div>
-            <div className='py-20'>
-                <OrderTracker activeStep={3}/>
-            </div>
-            <Grid className="space-x-5" container>
-                <Grid item container className='shadow-xl rounded-md p-5 border' sx={{alignItems:"center", justifyContent:'space-between'}}>
-                    <div>
-                        <img className="w-[5rem] h-[5rem] object-cover object-top" src="https://m.media-amazon.com/images/I/414872OyTsL.jpg" alt="Red One Piece Tshirt" />
-                    </div>
+  return (
+    <div className="px-5 lg:px-20">
+      {/* Delivery Address Section */}
+      <div>
+        <h1 className="font-semibold text-xl py-7">Delivery Address</h1>
+        <AddressCard />
+      </div>
 
-                    <div>
-                        <p className="">One Piece Monkey D Luffy Face Quote Anime T-Shirts for Men</p>
-                        <p><span>Color: Red </span> Size: M </p>
-                        <p>Seller: RahulBhai</p>
-                    </div>
-                </Grid>
+      <div className="py-20">
+        <OrderTracker activeStep={3} />
+      </div>
 
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Box
+            className="shadow-xl rounded-md p-5 border"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: "100%",
+            }}
+          >
+            <img
+              className="w-[5rem] h-[5rem] object-cover rounded-md mr-4"
+              src="https://m.media-amazon.com/images/I/414872OyTsL.jpg"
+              alt="Red One Piece Tshirt"
+            />
 
-            </Grid>
-        </div>
-    )
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <p className="font-semibold text-lg text-center mb-1">
+                One Piece Monkey D Luffy Face Quote Anime T-Shirts for Men
+              </p>
+              <p className="text-center mb-1">
+                <span className="mr-3">Color: Red</span>
+                <span>Size: M</span>
+              </p>
+              <p className="text-center mb-1">Seller: RahulBhai</p>
+              <p className="font-bold text-center">₹1099</p>
+            </Box>
+
+            <Box
+              sx={{
+                color: deepPurple[500],
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <StarBorderIcon sx={{ fontSize: "2rem", mr: 1 }} />
+              <span>Rate & Review Product</span>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default OrderDetails;
