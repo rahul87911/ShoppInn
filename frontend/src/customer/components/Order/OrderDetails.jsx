8b8f20040a2f2e8cc8805ab857787d8e021c36a9
varunkhadse <varunkhadse7@gmail.com>
2024-08-14 17:28:03 +0530

    minor changes

--- frontend/src/customer/components/Order/OrderDetails.jsx
+++ frontend/src/customer/components/Order/OrderDetails.jsx
@@ -5,7 +5,6 @@ import { Box, Grid } from "@mui/material";
 import { deepPurple } from "@mui/material/colors";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 
-
 const OrderDetails = () => {
   return (
     <div className="px-5 lg:px-20">
@@ -20,53 +19,56 @@ const OrderDetails = () => {
       </div>
 
       <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Box
-            className="shadow-xl rounded-md p-5 border"
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-              width: "100%",
-            }}
-          >
-            <img
-              className="w-[5rem] h-[5rem] object-cover rounded-md mr-4"
-              src="https://m.media-amazon.com/images/I/414872OyTsL.jpg"
-              alt="Red One Piece Tshirt"
-            />
-
+        {[1, 1, 1, 1, 1, 1].map((item, index) => (
+          <Grid item xs={12} key={index}>
             <Box
+              className="shadow-xl rounded-md p-5 border"
               sx={{
-                flexGrow: 1,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <p className="font-semibold text-lg text-center mb-1">
-                One Piece Monkey D Luffy Face Quote Anime T-Shirts for Men
-              </p>
-              <p className="text-center mb-1">
-                <span className="mr-3">Color: Red</span>
-                <span>Size: M</span>
-              </p>
-              <p className="text-center mb-1">Seller: RahulBhai</p>
-              <p className="font-bold text-center">₹1099</p>
-            </Box>
-
-            <Box
-              sx={{
-                color: deepPurple[500],
                 display: "flex",
                 alignItems: "center",
+                justifyContent: "space-between",
+                width: "100%",
               }}
             >
-              <StarBorderIcon sx={{ fontSize: "2rem", mr: 1 }} />
-              <span>Rate & Review Product</span>
+              <img
+                className="w-[5rem] h-[5rem] object-cover rounded-md mr-4"
+                src="https://m.media-amazon.com/images/I/414872OyTsL.jpg"
+                alt="Red One Piece Tshirt"
+              />
+
+              <Box
+                sx={{
+                  flexGrow: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start", // Align content to the left
+                }}
+              >
+                <p className="font-semibold text-lg text-left mb-1">
+                  One Piece Monkey D Luffy Face Quote Anime T-Shirts for Men
+                </p>
+                <p className="text-left mb-1">
+                  <span className="mr-3">Color: Red</span>
+                  <span>Size: M</span>
+                </p>
+                <p className="text-left mb-1">Seller: RahulBhai</p>
+                <p className="font-bold text-left">₹1099</p>
+              </Box>
+
+              <Box
+                sx={{
+                  color: deepPurple[500],
+                  display: "flex",
+                  alignItems: "center",
+                  flexDirection: "column", // Align content to the left
+                }}
+              >
+                <StarBorderIcon sx={{ fontSize: "2rem", mb: 1 }} />
+                <span className="text-left">Rate & Review Product</span>
+              </Box>
             </Box>
-          </Box>
-        </Grid>
+          </Grid>
+        ))}
       </Grid>
     </div>
   );
