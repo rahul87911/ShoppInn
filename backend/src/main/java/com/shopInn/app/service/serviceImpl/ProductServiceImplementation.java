17def692cbe029dffbf80f5f31ed233854aaf394
Rahul Mishra <afcatrahul8709@gmail.com>
2024-08-14 22:44:27 +0530

    product filter service impl done

--- backend/src/main/java/com/shopInn/app/service/serviceImpl/ProductServiceImplementation.java
+++ backend/src/main/java/com/shopInn/app/service/serviceImpl/ProductServiceImplementation.java
@@ -3,8 +3,12 @@ package com.shopInn.app.service.serviceImpl;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.shopInn.app.exception.ProductException;
@@ -208,8 +212,34 @@ public class ProductServiceImplementation implements ProductService {
 	@Override
 	public Page<Product> getAllProduct(String category, List<String> colors, List<String> sizes, Integer minPrice,
 			Integer maxPrice, Integer minDiscount, String sort, String stock, Integer pageNumber, Integer pageSize) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Pageable pageable=PageRequest.of(pageNumber, pageSize);
+		
+		List<Product> products=productRepository.filterProducts(category, minPrice, maxPrice, minDiscount, sort);
+		
+		if(!colors.isEmpty())
+		{
+			products=products.stream().filter(p->colors.stream().anyMatch(c->c.equalsIgnoreCase(p.getColor())))
+					.collect(Collectors.toList());
+		}
+		
+		if(stock!=null)
+		{
+			if(stock.equals("in_stock")) {
+			  products=products.stream().filter(p->p.getQuantity()>0).collect(Collectors.toList());	
+			}
+			else if(stock.equals("out_of_stock")) {
+			  products=products.stream().filter(p->p.getQuantity()<1).collect(Collectors.toList());
+			}
+		}
+		
+		int startIndex=(int)pageable.getOffset();
+		int endIndex=Math.min(startIndex+pageable.getPageSize(),products.size());
+		
+		List<Product>pageContent=products.subList(startIndex, endIndex);
+		
+		Page<Product>filteredProducts=new PageImpl<>(pageContent,pageable,products.size());
+		return filteredProducts;
 	}
 
 }
