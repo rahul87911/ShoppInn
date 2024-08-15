89b0883db00aeb15c6bff072fa9514142ca6236b
Rahul Mishra <afcatrahul8709@gmail.com>
2024-08-15 09:10:48 +0530

    cart item service implemented

--- backend/src/main/java/com/shopInn/app/repo/CartItemRepository.java
+++ backend/src/main/java/com/shopInn/app/repo/CartItemRepository.java
@@ -1,9 +1,15 @@
 package com.shopInn.app.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.shopInn.app.model.Cart;
 import com.shopInn.app.model.CartItem;
+import com.shopInn.app.model.Product;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
+	@Query("Select ci From CartItem ci Where ci.cart=:cart And ci.product=product And ci.size=:size And ci.userId=:userId")
+	public CartItem isCartItemExist(@Param("cart")Cart cart,@Param("product")Product product,@Param("size")String size,@Param("userId")Long userId);
 }
--- backend/src/main/java/com/shopInn/app/service/serviceImpl/CartItemServiceImplementation.java
+++ backend/src/main/java/com/shopInn/app/service/serviceImpl/CartItemServiceImplementation.java
@@ -1,5 +1,7 @@
 package com.shopInn.app.service.serviceImpl;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,7 @@ import com.shopInn.app.exception.UserException;
 import com.shopInn.app.model.Cart;
 import com.shopInn.app.model.CartItem;
 import com.shopInn.app.model.Product;
+import com.shopInn.app.model.User;
 import com.shopInn.app.repo.CartItemRepository;
 import com.shopInn.app.repo.CartRepository;
 import com.shopInn.app.service.CartItemService;
@@ -33,31 +36,63 @@ public class CartItemServiceImplementation implements CartItemService {
 	
 	@Override
 	public CartItem createCartItem(CartItem cartItem) {
-		// TODO Auto-generated method stub
-		return null;
+		cartItem.setQuantity(1);
+		cartItem.setPrice(cartItem.getProduct().getPrice()*cartItem.getQuantity());
+		cartItem.setDiscountedPrice(cartItem.getProduct().getDiscountedPrice()*cartItem.getQuantity());
+		
+		CartItem createdCartItem=cartItemRepository.save(cartItem)
+		
+		return createdCartItem;
 	}
 
 	@Override
 	public CartItem updateCartItem(Long userId, Long id, CartItem cartItem) throws CartItemException, UserException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		CartItem item=findCartItemById(id);
+		User user=userService.findUserById(item.getUserId());
+		
+		if(user.getId().equals(userId))
+		{
+			item.setQuantity(cartItem.getQuantity());
+			item.setPrice(item.getQuantity()*item.getProduct().getPrice());
+			item.setDiscountedPrice(item.getProduct().getDiscountedPrice()*item.getQuantity());
+		}
+		return cartItemRepository.save(item);
 	}
 
 	@Override
 	public CartItem isCartItemExist(Cart cart, Product product, String size, Long userId) {
-		// TODO Auto-generated method stub
-		return null;
+		CartItem cartItem=cartItemRepository.isCartItemExist(cart, product, size, userId);
+		
+		
+		return cartItem;
 	}
 
 	@Override
 	public void removeCartItem(Long userId, Long cartItemId) throws CartItemException, UserException {
-		// TODO Auto-generated method stub
+		CartItem cartItem=findCartItemById(cartItemId);
+		
+		User user=userService.findUserById(cartItem.getUserId());
+		
+		User reqUser=userService.findUserById(userId);
+		
+		if(user.getId().equals(reqUser.getId()))
+		{
+			cartItemRepository.deleteById(cartItemId);
+		}
+		else
+		throw new UserException("you can't remove another user's item"); 
 
 	}
 
 	@Override
 	public CartItem findCartItemById(Long cartItemId) throws CartItemException {
-		// TODO Auto-generated method stub
+		Optional<CartItem>opt=cartItemRepository.findById(cartItemId);
+		
+		if(opt.isPresent()) {
+			return opt.get();
+		}
+		throw new CartItemException("cartItem not found with id"+cartItemId);
 		return null;
 	}
 
