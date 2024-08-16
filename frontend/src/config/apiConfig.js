5ee9aa4c1cb73730d4f8ca2ec75a6df9dd11312c
Sagar <sagro93@gmail.com>
2024-08-16 18:32:22 +0530

    api config and Action, action type and reducer are made for products

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,1 +1,12 @@
 export const API_BASE_URL="http://localhost:8080"
+
+const jwt=localStorage.getItem("jwt")
+
+const api=axios.create({
+    baseURL:API_BASE_URL,
+    headers:{
+        "Authorization":`Bearer ${jwt}`,
+        "Content-Type":"application/json"
+
+    }
+})
