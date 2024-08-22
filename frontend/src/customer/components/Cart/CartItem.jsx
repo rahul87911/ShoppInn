c223f5a6facd3b6fb0c6d61f30c5d1cdb302e5c5
Rahul Mishra <afcatrahul8709@gmail.com>
2024-08-22 09:55:07 +0530

    api integeration done on 4 modules

--- frontend/src/customer/components/Cart/CartItem.jsx
+++ frontend/src/customer/components/Cart/CartItem.jsx
@@ -1,9 +1,37 @@
-import { IconButton } from '@mui/material';
+import { Button, IconButton } from '@mui/material';
 import React from 'react';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { useDispatch } from 'react-redux';
+import { removeCartItem, updateCartItem } from '../../../State/Cart/Action';
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const CartItem = ({item}) => {
+
+  const dispatch=useDispatch();
+
+  const handleUpdateCartItem=(num)=>{
+   const data={data:{quantity:item.quantity+num},cartItemId:item?.id}
+   dispatch(updateCartItem(data))
+  }
+
+  const handleRemoveCartItem=()=>{
+   dispatch(removeCartItem(item.id))
+  }
+
+
   return (
     <div className="flex items-start p-4 bg-white shadow-md border rounded-lg">
       <div className="flex-shrink-0 w-[100px] h-[100px] lg:w-[150px] lg:h-[150px] relative">
@@ -27,20 +55,21 @@ const CartItem = ({item}) => {
         </div>
         
         <div className="mt-4 flex items-center space-x-3">
-          <button className="text-blue-500 hover:underline text-sm">Remove</button>
+          <button onClick={handleRemoveCartItem} className="text-blue-500 hover:underline text-sm">Remove</button>
           <button className="text-blue-500 hover:underline text-sm">Save for later</button>
         </div>
       </div>
       <div className="flex-shrink-0 flex flex-col items-center ml-4">
         <div className="flex items-center space-x-2">
-          <IconButton>
+          <IconButton onClick={()=>handleUpdateCartItem(-1)} disabled={item.quantity<=1}>
             <RemoveCircleOutlineIcon />
           </IconButton>
-          <span className="py-1 px-4 border rounded-sm">1</span>
-          <IconButton>
+          <span className="py-1 px-4 border rounded-sm">{item.quantity}</span>
+          <IconButton onClick={()=>handleUpdateCartItem(1)}>
             <AddCircleOutlineIcon />
           </IconButton>
         </div>
+        
       </div>
     </div>
   );
